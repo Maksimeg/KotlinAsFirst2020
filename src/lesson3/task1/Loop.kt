6b9a26f99c2451e151a02e574e082d75ab310a4b
AndrewRedFox <90340969+AndrewRedFox@users.noreply.github.com>
2021-09-19 00:30:45 +0300

    fibSequenceDigit, squareSequenceDigit, fib tasks solver v 1.2

--- src/lesson3/task1/Loop.kt
+++ src/lesson3/task1/Loop.kt
@@ -222,7 +222,7 @@ fun sqr(n:Int):Int{
     return n*n
 }
 fun squareSequenceDigit(n: Int): Int {
-    var numbers=Array<Int>(20000,init = {i:Int->0})
+    var numbers=Array<Int>(50000,init = {i:Int->0})
     numbers[0]=1
     numbers[1]=4
     var numberMass:Int=2
