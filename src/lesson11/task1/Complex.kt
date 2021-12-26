b77ddb1961ba75108e154d69f73e36d8ce66b4c0
AndrewRedFox <90340969+AndrewRedFox@users.noreply.github.com>
2021-12-26 16:25:06 +0300

    task for 4 grade

--- src/lesson11/task1/Complex.kt
+++ src/lesson11/task1/Complex.kt
@@ -2,6 +2,8 @@
 
 package lesson11.task1
 
+import java.io.File
+
 /**
  * Фабричный метод для создания комплексного числа из строки вида x+yi
  */
@@ -57,4 +59,349 @@ class Complex(val re: Double, val im: Double) {
      * Преобразование в строку
      */
     override fun toString(): String = TODO()
+}
+
+fun main() {
+    println(game("input/game.txt", true))
+}
+
+fun game(inputName: String, xOr0: Boolean): String? {
+    val inputFile = File(inputName)
+    val field: Array<Array<Char>> = Array(15, { Array(15, { '0' }) })
+    var simvolInStr = 0
+    var countStr = 0
+    var kindOfSimbol: Char
+
+    if (xOr0) {
+        kindOfSimbol = 'x'
+    } else {
+        kindOfSimbol = '0'
+    }
+
+    for (line in inputFile.readLines()) {
+        for (symbol in line) {
+            field[countStr][simvolInStr] = symbol
+            simvolInStr += 1
+        }
+        countStr += 1
+        simvolInStr = 0
+    }
+
+    var provMassRightStr = Array(15, { 0 })
+    var provMassLeftStr = Array(15, { 0 })
+    var countOfXInStr = 0
+
+    //проверка строк
+    for (str in 0..14) {
+        for (i in 0..14) {
+            for (j in i..13) {
+                if (field[str][j + 1] == kindOfSimbol) {
+                    countOfXInStr += 1
+                    if (j == 13) {
+                        provMassRightStr[i] = countOfXInStr
+                        countOfXInStr = 0
+                        break
+                    }
+                } else {
+                    provMassRightStr[i] = countOfXInStr
+                    countOfXInStr = 0
+                    break
+                }
+            }
+        }
+        for (i in 14 downTo 1) {
+            for (j in i downTo 1) {
+                if (field[str][j - 1] == kindOfSimbol) {
+                    countOfXInStr += 1
+                    if (j == 1) {
+                        provMassRightStr[i] = countOfXInStr
+                        countOfXInStr = 0
+                        break
+                    }
+                } else {
+                    provMassLeftStr[i] = countOfXInStr
+                    countOfXInStr = 0
+                    break
+                }
+            }
+        }
+        for (j in 0..14) {
+            if (field[str][j] == '-' && provMassRightStr[j] + provMassLeftStr[j] > 3) {
+                return ((str + 1).toString() + " " + (j + 1).toString())
+            }
+        }
+    }
+    //проверка столбиков
+    for (stolb in 0..14) {
+        countOfXInStr = 0
+        for (i in 0..14) {
+            for (j in i..13) {
+                if (field[j + 1][stolb] == kindOfSimbol) {
+                    countOfXInStr += 1
+                    if (j == 13) {
+                        provMassRightStr[i] = countOfXInStr
+                        countOfXInStr = 0
+                        break
+                    }
+                } else {
+                    provMassRightStr[i] = countOfXInStr
+                    countOfXInStr = 0
+                    break
+                }
+            }
+        }
+        for (i in 14 downTo 1) {
+            for (j in i downTo 1) {
+                if (field[j - 1][stolb] == kindOfSimbol) {
+                    countOfXInStr += 1
+                    if (j == 1) {
+                        provMassRightStr[i] = countOfXInStr
+                        countOfXInStr = 0
+                        break
+                    }
+                } else {
+                    provMassLeftStr[i] = countOfXInStr
+                    countOfXInStr = 0
+                    break
+                }
+            }
+        }
+        for (j in 0..14) {
+            if (field[j][stolb] == '-' && provMassRightStr[j] + provMassLeftStr[j] > 3) {
+                return ((j + 1).toString() + " " + (stolb + 1).toString())
+            }
+        }
+    }
+
+//проверка диагоналей
+    countOfXInStr = 0
+     var str = 0
+    for (k in 0..10) {
+        for (i in 0..(14 - k)) {
+            str = i
+            for (j in i..(13 - k)) {
+                if (field[str + 1][j + 1 + k] == kindOfSimbol) {
+                    countOfXInStr += 1
+                    if (j == 13 - k) {
+                        provMassRightStr[i] = countOfXInStr
+                        countOfXInStr = 0
+                        break
+                    }
+                } else if (field[str + 1][j + 1 + k] != kindOfSimbol) {
+                    provMassRightStr[i] = countOfXInStr
+                    countOfXInStr = 0
+                    break
+                }
+                str += 1
+            }
+        }
+        for (i in (14 - k) downTo 1) {
+            str = i
+            for (j in (i + k) downTo 1) {
+                if (field[str - 1][j - 1] == kindOfSimbol) {
+                    countOfXInStr += 1
+                    if (j == 1 + k) {
+                        provMassLeftStr[i] = countOfXInStr
+                        countOfXInStr = 0
+                        break
+                    }
+                } else if (field[str - 1][j - 1] != kindOfSimbol) {
+                    provMassLeftStr[i] = countOfXInStr
+                    countOfXInStr = 0
+                    break
+                }
+                str -= 1
+            }
+        }
+        str = 0
+        var element = 0
+
+        while (element != 14 - k) {
+            if (field[str][element + k] == '-' && provMassRightStr[str] + provMassLeftStr[str] > 3) {
+                return ((str + 1).toString() + " " + (element + 1 + k).toString())
+            }
+            str += 1
+            element += 1
+        }
+
+    }
+
+    for (k in 1..10) {
+        for (i in 0..(14 - k)) {
+            str = i
+            for (j in i..(13 - k)) {
+                if (field[str + 1 + k][j + 1] == kindOfSimbol) {
+                    countOfXInStr += 1
+                    if (j == 13 - k) {
+                        provMassRightStr[i] = countOfXInStr
+                        countOfXInStr = 0
+                        break
+                    }
+                } else if (field[str + 1 + k][j + 1] != kindOfSimbol) {
+                    provMassRightStr[i] = countOfXInStr
+                    countOfXInStr = 0
+                    break
+                }
+                str += 1
+            }
+        }
+
+
+        for (i in (14 - k) downTo 1) {
+            str = i
+            for (j in (i - k) downTo 1) {
+                if (field[str - 1][j - 1] == kindOfSimbol) {
+                    countOfXInStr += 1
+                    if (str == 1 + k) {
+                        provMassLeftStr[i - k] = countOfXInStr
+                        countOfXInStr = 0
+                        break
+                    }
+                } else if (field[str - 1][j - 1] != kindOfSimbol) {
+                    provMassLeftStr[i - k] = countOfXInStr
+                    countOfXInStr = 0
+                    break
+                }
+                str -= 1
+            }
+        }
+        str = 0
+        var element = 0
+
+        while (element != 14 - k) {
+            if (field[str][element + k] == '-' && provMassRightStr[str] + provMassLeftStr[str] > 3) {
+                return ((str + 1 + k).toString() + " " + (element + 1).toString())
+            }
+            str += 1
+            element += 1
+        }
+    }
+     str = 0
+    var count = 0
+    countOfXInStr = 0
+    for (k in 0..10) {
+        count = 0
+        for (i in 14 downTo 1) {
+            str = i
+            for (j in count..13 - k) {
+                if (field[str - 1][j + 1 + k] == 'x') {
+                    countOfXInStr += 1
+                    if (j == 13 - k) {
+                        provMassRightStr[count] = countOfXInStr
+                        countOfXInStr = 0
+                        break
+                    }
+                } else if (field[str - 1][j + 1 + k] != kindOfSimbol) {
+                    provMassRightStr[count] = countOfXInStr
+                    countOfXInStr = 0
+                    break
+                }
+                str -= 1
+            }
+            count += 1
+        }
+
+
+        count = 14
+        for (i in 0..13 - k) {
+            str = i
+            for (j in count downTo 1) {
+                if (field[str + 1 + k][j - 1] == 'x') {
+                    countOfXInStr += 1
+                    if (j == 1 + k) {
+                        provMassLeftStr[count - k] = countOfXInStr
+                        countOfXInStr = 0
+                        break
+                    }
+                } else if (field[str + 1 + k][j - 1] != kindOfSimbol) {
+                    provMassLeftStr[count - k] = countOfXInStr
+                    countOfXInStr = 0
+                    break
+                }
+                str += 1
+
+            }
+            count -= 1
+        }
+        count = 0
+        str = 14
+        while (count != 14 - k) {
+            if (field[str][count + k] == '-' && provMassRightStr[count] + provMassLeftStr[count] > 3) {
+                return ((str + 1).toString() + " " + (count + 1 + k).toString())
+            }
+            str -= 1
+            count += 1
+        }
+    }
+
+    count = 0
+    for (k in 1..10) {
+        count = 0
+        for (i in 14 downTo 1) {
+            str = i
+            for (j in count..13 - k) {
+                if (field[str - 1 - k][j + 1] == 'x') {
+                    countOfXInStr += 1
+                    if (j == 13 - k) {
+                        provMassRightStr[count] = countOfXInStr
+                        countOfXInStr = 0
+                        break
+                    }
+                } else if (field[str - 1 - k][j + 1] != kindOfSimbol) {
+                    provMassRightStr[count] = countOfXInStr
+                    countOfXInStr = 0
+                    break
+                }
+                str -= 1
+            }
+            count += 1
+        }
+
+        count = 14
+        for (i in 0..13 - k) {
+            str = i
+            for (j in count downTo 1) {
+                if (field[str + 1][j - 1 - k] == 'x') {
+                    countOfXInStr += 1
+                    if (j == 1 + k) {
+                        provMassLeftStr[count - k] = countOfXInStr
+                        countOfXInStr = 0
+                        break
+                    }
+                } else if (field[str + 1][j - 1 - k] != kindOfSimbol) {
+                    provMassLeftStr[count - k] = countOfXInStr
+                    countOfXInStr = 0
+                    break
+                }
+                str += 1
+
+            }
+            count -= 1
+        }
+
+        count = 0
+        str = 13
+        while (count != 14 - k) {
+            if (field[str][count] == '-' && provMassRightStr[count] + provMassLeftStr[count] > 3) {
+                return ((str + 2 - k).toString() + " " + (count + 1).toString())
+            }
+            str -= 1
+            count += 1
+        }
+    }
+
+
+    /*for (i in 0..14) {
+        print(provMassRightStr[i])
+    }
+    println()*/
+
+    /*for (i in 0..14) {
+        for (j in 0..14) {
+            print(field[i][j])
+        }
+        println()
+    }*/
+
+    return null
 }
